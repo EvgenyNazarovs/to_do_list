d693f398d6524d11e4df5642a35d0fbb3c047433
Evgeny Nazarovs <eugene.nazarovs@gmail.com>
2020-04-20 21:35:27 +0100

    can add items to the list

--- src/app.js
+++ src/app.js
@@ -2,6 +2,17 @@ import Vue from 'vue';
 
 document.addEventListener('DOMContentLoaded', () => {
   new Vue({
-    el: "#app"
-  })
-})
+    el: "#app",
+    data: {
+      items: ["Clean flat", "Take out rubbish", "Pay rent"],
+      newItem:  ""
+    },
+    methods: {
+      saveNewItem: function(){
+        const itemToAdd = this.newItem;
+        this.items.push(itemToAdd);
+        this.newItem = "";
+      }
+    }
+  });
+});
